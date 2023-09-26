ecd96c02e08d6bab2a75e5750d191567f571ea86
Jweiler94 <jweiler94@hotmail.com>
2023-09-25 18:56:51 -0600

    commit during work

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,12 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+
 public class JobTest {
+
+    @Test
+        public void testSettingJobId() {
+
+    }
     //TODO: Create your unit tests here
 }
