bc73e256fff8d932f40a318189893553add4b895
Jweiler94 <jweiler94@hotmail.com>
2023-09-26 19:56:45 -0600

    small change to testing

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -3,6 +3,8 @@ package org.launchcode.techjobs.oo;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 
+import javax.swing.text.Position;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.testng.Assert.assertTrue;
@@ -26,17 +28,17 @@ public class JobTest {
 
         Job testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
 
-        assertTrue(testJob.getEmployer() != null);
-        assertTrue(testJob.getLocation() != null);
-        assertTrue(testJob.getPositionType() != null);
-        assertTrue(testJob.getCoreCompetency() != null);
-        assertTrue(testJob.getName() != null);
+        assertTrue(testJob.getEmployer() instanceof Employer);
+        assertTrue(testJob.getLocation() instanceof Location);
+        assertTrue(testJob.getPositionType() instanceof PositionType);
+        assertTrue(testJob.getCoreCompetency() instanceof CoreCompetency);
+        assertTrue(testJob.getName() instanceof String);
 
         assertEquals(testJob.getEmployer().toString(), "ACME");
         assertEquals(testJob.getLocation().toString(), "Desert");
         assertEquals(testJob.getPositionType().toString(), "Quality control");
         assertEquals(testJob.getCoreCompetency().toString(), "Persistence");
-
+        assertEquals(testJob.getName(), "Product tester");
     }
     //TODO: Create your unit tests here
 }
